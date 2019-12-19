7de0882af12a0a71f7fbea7b8601b8781f070b50
dee4git <57084424+dee4git@users.noreply.github.com>
2019-12-20 05:58:45 +0600

    App added "accounts" fully functional log in/log out system added reg.html added login.html added

---
+++ mysite/accounts/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('reg', views.reg, name='reg'),
+    path('login', views.login, name='login'),
+    path('logout', views.logout, name='logout'),
+    path('home', views.home, name='home'),
+
+]
--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -1,10 +1,3 @@
-from django.http import HttpResponse
 from django.shortcuts import render
-def about (request):
-    #return HttpResponse("About is working")
-    return render(request,'about.html')
-def home (request) :
-    #return HttpResponse("This is a homepage")
-    return render(request,'home.html')
-def makeMoney (request) :
-    return render (request,'makeMoney.html')
+def home (request):
+    return render (request,'home.html')
